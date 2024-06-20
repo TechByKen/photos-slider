9f86bb73328ca122778f385a20d6e6b70ca83880
TechByKen <diala.kenneth98@gmail.com>
2024-06-19 23:18:01 -0700

    Worked on the script file

--- script.js
+++ script.js
@@ -4,15 +4,31 @@ const imgContainer = document.querySelector(".img-container");
 const imgs = document.querySelectorAll("img");
 
 let currentImg = 1;
+let timeout;
 
 nextBtn.addEventListener("click", () => {
   currentImg++;
+  clearTimeout(timeout);
   updateImg();
 });
+
+prevBtn.addEventListener("click", () => {
+  currentImg--;
+  clearTimeout(timeout);
+  updateImg();
+});
+
+updateImg();
 
 function updateImg() {
   if (currentImg > imgs.length) {
     currentImg = 1;
+  } else if (currentImg < 1) {
+    currentImg = imgs.length;
   }
   imgContainer.style.transform = `translateX(-${(currentImg - 1) * 500}px)`;
+  timeout = setTimeout(() => {
+    currentImg++;
+    updateImg();
+  }, 3000);
 }
